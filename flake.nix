626a71ce46149d7aa0abf137a2962aa6b8406efc
frosk <hz@mail.com>
2025-07-22 00:00:46 +0300

    added quickshell

--- flake.nix
+++ flake.nix
@@ -3,23 +3,28 @@
   description = "zaebis flake";
 
   inputs = {
-   home-manager.url = "github:nix-community/home-manager/master"; 
-   home-manager.inputs.nixpkgs.follows = "nixpkgs";
-   nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master"; 
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
 
-   hyprland.url = "github:hyprwm/Hyprland";
-   hyprland-plugins = {
-     url = "github:hyprwm/hyprland-plugins";
-     inputs.hyprland.follows = "hyprland";
-   };
-   hypr-dynamic-cursors = {
-      url = "github:VirtCode/hypr-dynamic-cursors";
+    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
-   };
+    };
+    hypr-dynamic-cursors = {
+       url = "github:VirtCode/hypr-dynamic-cursors";
+       inputs.hyprland.follows = "hyprland";
+    };
 
-   nixcats.url = "path:/home/frosk/.dotfiles/programs/system/nvim";
-   swww.url = "github:LGFae/swww";
-  };
+    quickshell = {
+       url = "github:quickshell-mirror/quickshell";
+       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixcats.url = "path:/home/frosk/.dotfiles/programs/system/nvim";
+    swww.url = "github:LGFae/swww";
+   };
 
   outputs = { self, nixpkgs, home-manager, hyprland, ... }@inputs: 
     let
