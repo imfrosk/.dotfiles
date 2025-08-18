6ba3a03135e370e989a3d9e308221285592775de
frosk <hz@mail.com>
2025-08-18 22:49:39 +0300

    add sunshine

--- programs/home/sunshine/default.nix
+++ programs/home/sunshine/default.nix
@@ -8,10 +8,35 @@
   config = lib.mkIf config.sunshine.enable {
     services.sunshine = {
       enable = true;
+      package = pkgs.sunshine.override { cudaSupport = true; };
       capSysAdmin = true;
+      openFirewall = true;
       settings = {
         sunshine_name = "nixos";
+        output_name = "1";
+        #capture = "wlr";
+        #encoder = "software";
+        adapter_name = "/dev/dri/renderD128";
       };
+      applications = {
+        env = {
+          PATH = "$(PATH):$(HOME)/.local/bin";
+        };
+        apps = [
+          {
+            name = "Second screen";
+            prep-cmd = [
+              {
+                do = "hyprctl output create headless PM-HEADLESS";
+                undo = "hyprctl output remove PM-HEADLESS";
+              }
+            ];
+            exclude-global-prep-cmd = "false";
+            auto-detach = "true";
+          }
+        ];
+      };
+      
     };
   };
 }
